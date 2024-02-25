a417afdd89c9b9496956656abbce706c42425a19
hasanm.hasan93 <hasanm.hasan93@gmail.com>
2024-02-25 15:41:39 +0300

    add space

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -14,8 +14,9 @@
       height="50px"
       class="d-flex align-center justify-center"
     >
-      {{ new Date().getFullYear() }} , made with
-      <v-icon color="red">mdi-heart</v-icon> by our Team &copy; , SVU - ITE
+      {{ new Date().getFullYear() }} , made with &nbsp;
+      <v-icon color="red">mdi-heart</v-icon> &nbsp;by our Team &copy; , SVU -
+      ITE
     </v-card>
   </v-footer>
 </template>
